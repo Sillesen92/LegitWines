1a85f194710e6e753378d7564f0370acd61ec397
Jakob Fejerskov <eaajmbfejerskov@students.eaaa.dk>
2021-11-17 12:41:59 +0100

    require repository til test

--- test/UserStory1Test.spec.js
+++ test/UserStory1Test.spec.js
@@ -2,6 +2,7 @@ const Booking = require('../model/Booking')
 const Customer = require('../model/Customer')
 const Salesman = require('../model/Salesman')
 const BookingController = require('../controller/bookings')
+const repository = require('../repository/repository')
 const Company = require('../model/Company')
 const newCustomer = null;
 const newSalesman = null;
@@ -26,7 +27,7 @@ describe('Unit test af Booking klasse', () => {
         const salesId = "FunkMasterJones"
         newSalesman = new Salesman(name, salesEmail, salesPhoneNr, salesId)
     })
-    
+
     test('create booking', () => {
         // preparation of booking: 
         const bookingNr = 20210001;
@@ -51,40 +52,40 @@ describe('Unit test af Booking klasse', () => {
         const hotEmail = "test@test.dk"
         const hotPhone = 55555555
         const hotBusinessType = "hotel"
-        const newHotel = new Company(hotName,HotAdress,hotEmail,hotPhone,hotBusinessType)
+        const newHotel = new Company(hotName, HotAdress, hotEmail, hotPhone, hotBusinessType)
         //airline
-        const airName ="airline"
+        const airName = "airline"
         const airAdress = "test 123"
         const airEmail = "test@test.dk"
         const airPhone = 55555555
         const airBusinessType = "airline"
-        const newAirline = new Company(airName,airAdress,airEmail,airPhone,airBusinessType)
+        const newAirline = new Company(airName, airAdress, airEmail, airPhone, airBusinessType)
         //bus
-        const busName ="bus"
+        const busName = "bus"
         const busAdress = "test 123"
         const busEmail = "test@test.dk"
         const busPhone = 55555555
         const busBusinessType = "bus"
-        const newBus = new Company(busName,busAdress,busEmail,busPhone,busBusinessType)
+        const newBus = new Company(busName, busAdress, busEmail, busPhone, busBusinessType)
 
         //billeje
-        const bilName ="bil"
+        const bilName = "bil"
         const bilAdress = "test 123"
         const bilEmail = "test@test.dk"
         const bilPhone = 55555555
         const bilBusinessType = "bil"
-        const newBil = new Company(bilName,bilAdress,bilEmail,bilPhone,bilBusinessType)
+        const newBil = new Company(bilName, bilAdress, bilEmail, bilPhone, bilBusinessType)
 
         //golf
-        const golfName ="golf"
+        const golfName = "golf"
         const golfAdress = "test 123"
         const golfEmail = "test@test.dk"
         const golfPhone = 55555555
         const golfBusinessType = "golf"
-        const newgolf = new Company(golfName,golfAdress,golfEmail,golfPhone,golfBusinessType)
-        
+        const newgolf = new Company(golfName, golfAdress, golfEmail, golfPhone, golfBusinessType)
+
         //todo -lav en af hver 
-        
+
         //act
         const result = newBooking.calcGrossPrice();
 
