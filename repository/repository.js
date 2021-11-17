cf5c1946135c8a9406aa2a10d8258ab61733fdeb
DataAnd <andersl123@hotmail.com>
2021-11-17 20:34:11 +0100

    saveBooking semi done

--- repository/repository.js
+++ repository/repository.js
@@ -10,17 +10,28 @@ const db = admin.firestore();
 
 //Mangler de gyldige parametre
 /*
-async function saveBooking(bookingNr){
+async function saveBooking(bookingNr, grossPrice, contributionMargin, salesman, reservations, transfers, customer, customers, carRentals, greenFees){
   {
     const booking = {
-      bookingNr = bookingNr
+      bookingNr = bookingNr,
+      grossPrice = grossPrice,
+      contributionMargin = contributionMargin,
+      salesman = salesman,
+      reservations = reservations,
+      transfers = transfers,
+      customer = customer,
+      passengers = passengers,
+      carRentals = carRentals,
+      greenFees = greenFees
   }
+  const bookingJson = JSON.stringify(booking)
+  
 }const doc = await db.collection("Bookings").doc()
 await doc.set(booking)
 return doc.id
 
 }
-//Henter specific booking på bookingId
+//Henter specific booking på bookingNr
 
 async function getBooking(bookingNr){
   try{
