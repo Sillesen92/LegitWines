9c548f830aeca00cfc1fcabc52e13e28910e624c
JonasSillesen <sillesen92@gmail.com>
2021-11-21 12:44:40 +0100

    SalesmanRoute og gui til oprættelse af sælger er oprettet

---
+++ routes/createSalesmanRoute.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const salesmanController = require('../controller/salesmen.js')
+const router = express.Router();
+
+router.get('/createSalesman', async (req, resp) => {
+        resp.render('createSalesman')
+})
+
+router.post('/createSalesman', async (req, resp) => {
+        try {
+                const {salesmanName, salesmanEmail, salesmanPhoneNr, salesmanSalesId, salesmanPassword} = req.body;
+                const createSuccess = await salesmanController.createSalesman(salesmanName, salesmanEmail, salesmanPhoneNr, salesmanSalesId, salesmanPassword);
+                if (createSuccess) {
+                        resp.sendStatus(200);
+                } else {
+                        resp.status(400).json({error: "Fejl under oprettelse af sælger"});
+                }
+        } catch (error) {
+                console.log(error)
+                resp.status(400).json(error);
+        }
+})
+module.exports = router
